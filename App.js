1974d1fcd39dcbf36417a32dd1f1ecce35988bfa
Laksara <it21092824@my.sliit.lk>
2023-03-04 09:31:37 +0530

    added read feature

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
 console.log("App.js is running!");
 
 const fs = require("fs");
-fs.writeFile("file.txt", "Hello World!", function (err) {
+fs.readFile("file.txt", "utf8", function (err, data) {
+  if (err) throw err;
+  console.log(data);
+  console.log("log 1");
+});
+
+//const fs = require("fs");
+fs.writeFile("file.txt", "Hello World!2", function (err) {
   if (err) throw err;
   console.log("File saved!");
+  console.log("log 2");
 });
